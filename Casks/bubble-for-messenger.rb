a2a88174ab0384e1fbcbdfeda23d7efedf905446
Alexandros Christou <56453720+termite09@users.noreply.github.com>
2026-09-13 16:25:53 +0300

    Update Bubble cask to v2.1.0

--- Casks/bubble-for-messenger.rb
+++ Casks/bubble-for-messenger.rb
@@ -1,10 +1,10 @@
 cask "bubble-for-messenger" do
   version "2.1.0"
-  sha256 "90b1db2bff69423eb6a3acc2e3addd243f35f2cf51fcfa358d070faeaf5b16e2"
+  sha256 "0ecdf39bc26b40ac73d83160fe30d5c7b21bc4119e767959629e993f0f863d1d"
 
   url "https://github.com/termite09/bubble-for-messenger/releases/download/v#{version}/Bubble-#{version}-arm64.dmg"
   name "Bubble for Messenger"
-  desc "Facebook Messenger as a floating chat head"
+  desc "Facebook Messenger as a floating chat head for macOS"
   homepage "https://github.com/termite09/bubble-for-messenger"
 
   livecheck do
